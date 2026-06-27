67474d22a2181e8d72210ff45e9d9e390eab93b6
“abiiayyyasgit config --global user.email “abiiayyas@gmail.com <user@MacBook-Pro-user.local>
2026-06-27 23:59:40 +0700

    fix: resize login page logo for better mobile view

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <img src="{{ asset('assets/logo.png') }}" class="h-20 w-auto object-contain" alt="Diginiaga Logo">
+                    <img src="{{ asset('assets/logo.png') }}" class="h-10 sm:h-14 w-auto object-contain" alt="Diginiaga Logo">
                 </a>
             </div>
 
